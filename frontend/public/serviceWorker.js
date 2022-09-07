f27b0f7f7a44cbd7031c3b783127d989f39a061f
爬山過海肥肥豬 <smartbean@KC-MacBook-Pro.local>
2022-09-07 14:06:13 +0800

    PWA done

--- frontend/public/serviceWorker.js
+++ frontend/public/serviceWorker.js
@@ -1,44 +1,78 @@
-const CACHE_NAME = "version-1";
-const urlsToCache = ["offline.html"];
+const OFFLINE_VERSION = 1;
+const CACHE_NAME = "offline";
 
-const self = this;
+const OFFLINE_URL = "offline.html";
 
-// Install SW
+//Install SW
 self.addEventListener("install", (event) => {
     event.waitUntil(
-        caches.open(CACHE_NAME).then((cache) => {
-            console.log("Opened cache");
-
-            return cache.addAll(urlsToCache);
-        })
+        (async () => {
+            const cache = await caches.open(CACHE_NAME);
+            // Setting {cache: 'reload'} in the new request ensures that the
+            // response isn't fulfilled from the HTTP cache; i.e., it will be
+            // from the network.
+            await cache.add(new Request(OFFLINE_URL, { cache: "reload" }));
+        })()
     );
+    // Force the waiting service worker to become the active service worker.
+    self.skipWaiting();
+});
+
+//Activate SW
+self.addEventListener("activate", (event) => {
+    event.waitUntil(
+        (async () => {
+            // Enable navigation preload if it's supported.
+            if ("navigationPreload" in self.registration) {
+                await self.registration.navigationPreload.enable();
+            }
+        })()
+    );
+
+    // Tell the active service worker to take control of the page immediately.
+    self.clients.claim();
 });
 
 // Listen for requests
 self.addEventListener("fetch", (event) => {
-    event.respondWith(
-        caches.match(event.request).then(() => {
-            return fetch(event.request).catch(() =>
-                caches.match("offline.html")
-            );
-        })
-    );
-});
-
-// Activate the SW
-self.addEventListener("activate", (event) => {
-    const cacheWhitelist = [];
-    cacheWhitelist.push(CACHE_NAME);
-
-    event.waitUntil(
-        caches.keys().then((cacheNames) =>
-            Promise.all(
-                cacheNames.map((cacheName) => {
-                    if (!cacheWhitelist.includes(cacheName)) {
-                        return caches.delete(cacheName);
+    // Only call event.respondWith() if this is a navigation request
+    // for an HTML page.
+    if (event.request.mode === "navigate") {
+        event.respondWith(
+            (async () => {
+                try {
+                    // First, try to use the navigation preload response if it's
+                    // supported.
+                    const preloadResponse = await event.preloadResponse;
+                    if (preloadResponse) {
+                        return preloadResponse;
                     }
-                })
-            )
-        )
-    );
+
+                    // Always try the network first.
+                    const networkResponse = await fetch(event.request);
+                    return networkResponse;
+                } catch (error) {
+                    // catch is only triggered if an exception is thrown, which is
+                    // likely due to a network error.
+                    // If fetch() returns a valid HTTP response with a response code in
+                    // the 4xx or 5xx range, the catch() will NOT be called.
+                    console.log(
+                        "Fetch failed; returning offline page instead.",
+                        error
+                    );
+
+                    const cache = await caches.open(CACHE_NAME);
+                    const cachedResponse = await cache.match(OFFLINE_URL);
+                    return cachedResponse;
+                }
+            })()
+        );
+    }
+
+    // If our if() condition is false, then this fetch handler won't
+    // intercept the request. If there are any other fetch handlers
+    // registered, they will get a chance to call event.respondWith().
+    // If no fetch handlers call event.respondWith(), the request
+    // will be handled by the browser as if there were no service
+    // worker involvement.
 });
