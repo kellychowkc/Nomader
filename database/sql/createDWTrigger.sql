2eedc6dcdacf92a83c60b904341e322640b4a9aa
samshing123 <samshingh@gmail.com>
2022-09-22 17:47:09 +0800

    updagte users table

--- database/sql/createDWTrigger.sql
+++ database/sql/createDWTrigger.sql
@@ -1,325 +1,249 @@
-CREATE OR REPLACE FUNCTION insert_staging_countries()
-RETURNS TRIGGER
-LANGUAGE plpgsql
-AS $$
-    BEGIN
-        INSERT INTO dim_countries
-            (country_name)
-            VALUES (NEW.name);
-        RETURN NEW;
-    END
-$$;
 
-CREATE TRIGGER trigger_insert_staging_countries
-AFTER INSERT ON staging_countries
-FOR EACH ROW EXECUTE PROCEDURE insert_staging_countries();
+CREATE TABLE staging_users (
+    id SERIAL PRIMARY KEY,
+    birthday_year INTEGER,
+    birthday_month INTEGER,
+    birthday_day INTEGER,
+    gender TEXT,
+    job_id INTEGER,
+    isAdmin BOOLEAN,
+    cuntry_id INTEGER,
+    created_year INTEGER,
+    created_month INTEGER,
+    created_day INTEGER
+);
 
+CREATE TABLE staging_jobs (
+    id SERIAL PRIMARY KEY,
+    title TEXT
+); 
 
+CREATE TABLE staging_interests (
+    id SERIAL PRIMARY KEY,
+    title TEXT
+);
 
-CREATE OR REPLACE FUNCTION insert_staging_cities()
-RETURNS TRIGGER
-LANGUAGE plpgsql
-AS $$
-    BEGIN
-        INSERT INTO dim_cities
-            (city_name, city_list)
-            VALUES (NEW.name, NEW.city_list);
-        RETURN NEW;
-    END
-$$;
+CREATE TABLE staging_users_interests (
+    id SERIAL PRIMARY KEY,
+    user_id INTEGER,
+    interest_id INTEGER
+);
 
-CREATE TRIGGER trigger_insert_staging_cities
-AFTER INSERT ON staging_cities
-FOR EACH ROW EXECUTE PROCEDURE insert_staging_cities();
+CREATE TABLE staging_chat_rooms (
+    id SERIAL PRIMARY KEY,
+    created_year INTEGER,
+    created_month INTEGER,
+    created_day INTEGER
+);
 
+CREATE TABLE staging_chats (
+    id SERIAL PRIMARY KEY,
+    chat_room_id INTEGER,
+    created_year INTEGER,
+    created_month INTEGER,
+    created_day INTEGER
+);
 
+CREATE TABLE staging_users_relationship (
+    id SERIAL PRIMARY KEY,
+    user1_id INTEGER,
+    user2_id INTEGER,
+    created_year INTEGER,
+    created_month INTEGER,
+    created_day INTEGER
+);
 
-CREATE OR REPLACE FUNCTION insert_staging_attractions()
-RETURNS TRIGGER
-LANGUAGE plpgsql
-AS $$
-    BEGIN
-        INSERT INTO dim_attractions
-            (attraction_name, city_list)
-            VALUES (NEW.name, NEW.city_list);
-        RETURN NEW;
-    END
-$$;
+CREATE TABLE staging_browse_attractions (
+    id SERIAL PRIMARY KEY,
+    user_id INTEGER,
+    browse_count INTEGER,
+    attraction_id INTEGER,
+    created_year INTEGER,
+    created_month INTEGER,
+    created_day INTEGER 
+);
 
-CREATE TRIGGER trigger_insert_staging_attractions
-AFTER INSERT ON staging_attractions
-FOR EACH ROW EXECUTE PROCEDURE insert_staging_attractions();
+CREATE TABLE staging_countries (
+    id SERIAL PRIMARY KEY,
+    name TEXT
+);
 
+CREATE TABLE staging_cities (
+    id SERIAL PRIMARY KEY,
+    name TEXT,
+    city_list TEXT
+);
 
+CREATE TABLE staging_attractions (
+    id SERIAL PRIMARY KEY,
+    name TEXT,
+    city_list TEXT
+);
 
-CREATE OR REPLACE FUNCTION insert_staging_interests()
-RETURNS TRIGGER
-LANGUAGE plpgsql
-AS $$
-    BEGIN
-        INSERT INTO dim_interests
-            (title)
-            VALUES (NEW.title);
-        RETURN NEW;
-    END
-$$;
+CREATE TABLE staging_posts (
+    id SERIAL PRIMARY KEY,
+    user_id INTEGER,
+    city_id INTEGER,
+    attraction_id INTEGER,
+    created_year INTEGER,
+    created_month INTEGER,
+    created_day INTEGER
+);
 
-CREATE TRIGGER trigger_insert_staging_interests
-AFTER INSERT ON staging_interests
-FOR EACH ROW EXECUTE PROCEDURE insert_staging_interests();
+CREATE TABLE staging_browse_posts (
+    id SERIAL PRIMARY KEY,
+    user_id INTEGER,
+    browse_count INTEGER,
+    post_id INTEGER
+);
 
-
-
-CREATE OR REPLACE FUNCTION insert_staging_jobs()
-RETURNS TRIGGER
-LANGUAGE plpgsql
-AS $$
-    BEGIN
-        INSERT INTO dim_jobs
-            (title)
-            VALUES (NEW.title);
-        RETURN NEW;
-    END
-$$;
-
-CREATE TRIGGER trigger_insert_staging_jobs
-AFTER INSERT ON staging_jobs
-FOR EACH ROW EXECUTE PROCEDURE insert_staging_attractions();
-
-
-CREATE OR REPLACE FUNCTION insert_staging_chat_rooms()
-RETURNS TRIGGER
-LANGUAGE plpgsql
-AS $$
-    DECLARE
-        created_date_id INTEGER;
-    BEGIN
-        INSERT INTO dim_dates
-            (year, month, day)
-            VALUES (NEW.created_year, NEW.created_month, NEW.created_day)
-            ON CONFLICT(year, month, day)
-            DO NOTHING
-            RETURNING id
-            INTO created_date_id;
-        INSERT INTO fact_chat_rooms
-            (created_date_id)
-            VALUES (created_date_id);
-        RETURN NEW;
-    END
-$$;
-
-CREATE TRIGGER trigger_insert_staging_chat_rooms
-AFTER INSERT ON staging_chat_rooms
-FOR EACH ROW EXECUTE PROCEDURE insert_staging_chat_rooms();
+CREATE TABLE staging_posts_type (
+    id SERIAL PRIMARY KEY,
+    post_id INTEGER,
+    interest_id INTEGER
+);
 
 
 
-CREATE OR REPLACE FUNCTION insert_staging_chats()
-RETURNS TRIGGER
-LANGUAGE plpgsql
-AS $$
-    DECLARE
-        created_date_id INTEGER;
-    BEGIN
-        INSERT INTO dim_dates
-            (year, month, day)
-            VALUES (NEW.created_year, NEW.created_month, NEW.created_day)
-            ON CONFLICT(year, month, day)
-            DO NOTHING
-            RETURNING id
-            INTO created_date_id;
-        INSERT INTO fact_chats
-            (chat_room_id, created_date_id)
-            VALUES (NEW.chat_room_id, created_date_id);
-        RETURN NEW;
-    END
-$$;
+CREATE TABLE dim_countries (
+    id SERIAL PRIMARY KEY,
+    country_name TEXT
+);
+CREATE UNIQUE INDEX countries_unique_idx on dim_countries(country_name);
 
-CREATE TRIGGER trigger_insert_staging_chats
-AFTER INSERT ON staging_chats
-FOR EACH ROW EXECUTE PROCEDURE insert_staging_chats();
+CREATE TABLE dim_cities (
+    id SERIAL PRIMARY KEY,
+    city_name TEXT,
+    city_list TEXT
+);
+CREATE UNIQUE INDEX cities_unique_idx on dim_cities(city_name, city_list);
 
+CREATE TABLE dim_attractions (
+    id SERIAL PRIMARY KEY,
+    attraction_name TEXT,
+    city_list TEXT
+);
+CREATE UNIQUE INDEX attractons_unique_idx on dim_attractions(attraction_name, city_list);
 
+CREATE TABLE dim_interests (
+    id SERIAL PRIMARY KEY,
+    interest_name TEXT
+);
+CREATE UNIQUE INDEX interests_unique_idx on dim_interests(interest_name);
 
-CREATE OR REPLACE FUNCTION insert_staging_users()
-RETURNS TRIGGER
-LANGUAGE plpgsql
-AS $$
-    DECLARE
-        birthday_id INTEGER;
-        created_date_id INTEGER;
-        gender_id INTEGER;
-    BEGIN
-        INSERT INTO dim_genders
-            (status)
-            VALUES (NEW.gender)
-            ON CONFLICT(status)
-            DO NOTHING
-            RETURNING id
-            INTO gender_id;
-        INSERT INTO dim_dates
-            (year, month, day)
-            VALUES (NEW.birthday_year, NEW.birthday_month, NEW.birthday_day)
-            ON CONFLICT(year, month, day)
-            DO NOTHING
-            RETURNING id
-            INTO birthday_id;
-        INSERT INTO dim_dates
-            (year, month, day)
-            VALUES (NEW.created_year, NEW.created_month, NEW.created_day)
-            ON CONFLICT(year, month, day)
-            DO NOTHING
-            RETURNING id
-            INTO created_date_id;
-        INSERT INTO fact_users_data 
-            (country_id, interest_id, birthday_id, created_date_id, job_id, gender_id)
-            VALUES (NEW.country_id, NEW.interest_id, birthday_id, created_date_id, NEW.job_id, gender_id);
-        RETURN NEW;
-    END
-$$;
+CREATE TABLE dim_jobs (
+    id SERIAL PRIMARY KEY,
+    job_title TEXT
+);
+CREATE UNIQUE INDEX jobs_unique_idx on dim_jobs(job_title);
+ 
+CREATE TABLE dim_genders (
+    id SERIAL PRIMARY KEY,
+    status TEXT
+);
+CREATE UNIQUE INDEX genders_unique_idx on dim_genders(status);
 
-CREATE TRIGGER trigger_insert_staging_users
-AFTER INSERT ON staging_users
-FOR EACH ROW EXECUTE PROCEDURE insert_staging_users();
+CREATE TABLE dim_dates (
+    id SERIAL PRIMARY KEY,
+    year TEXT,
+    month TEXT,
+    day TEXT
+);
+CREATE UNIQUE INDEX dates_unique_idx on dim_dates (year, month, day);
 
 
 
+CREATE TABLE fact_attractions_interests (
+    id SERIAL PRIMARY KEY,
+    attraction_id INTEGER,
+    FOREIGN KEY (attraction_id) REFERENCES dim_attractions(id),
+    interest_id INTEGER,
+    FOREIGN KEY (interest_id) REFERENCES dim_interests(id)
+);
 
-CREATE OR REPLACE FUNCTION insert_staging_users_relationship()
-RETURNS TRIGGER
-LANGUAGE plpgsql
-AS $$
-    DECLARE
-        created_date_id INTEGER;
-    BEGIN
-        INSERT INTO dim_dates
-            (year, month, day)
-            VALUES (NEW.created_year, NEW.created_month, NEW.created_day)
-            ON CONFLICT(year, month, day)
-            DO NOTHING
-            RETURNING id
-            INTO created_date_id;
-        INSERT INTO fact_users_relactionship
-            (user1_id, user2_id, created_date_id)
-            VALUES (NEW.user1_id, NEW.user2_id, created_date_id);
-        RETURN NEW;
-    END
-$$;
+CREATE TABLE fact_chat_rooms (
+    id SERIAL PRIMARY KEY,
+    created_date_id INTEGER,
+    FOREIGN KEY (created_date_id) REFERENCES dim_dates(id)
+);
 
-CREATE TRIGGER trigger_insert_staging_users_relactionship
-AFTER INSERT ON staging_users_relationship
-FOR EACH ROW EXECUTE PROCEDURE insert_staging_users_relationship();
+CREATE TABLE fact_chats (
+    id SERIAL PRIMARY KEY,
+    created_date_id INTEGER,
+    FOREIGN KEY (created_date_id) REFERENCES dim_dates(id),
+    chat_room_id INTEGER,
+    FOREIGN KEY (chat_room_id) REFERENCES fact_chat_rooms(id)
+);
 
+CREATE TABLE fact_users (
+    id SERIAL PRIMARY KEY,
+    birthday_id INTEGER,
+    FOREIGN KEY (birthday_id) REFERENCES dim_dates(id),
+    created_date_id INTEGER,
+    FOREIGN KEY (created_date_id) REFERENCES dim_dates(id),
+    country_id INTEGER,
+    FOREIGN KEY (country_id) REFERENCES dim_countries(id),
+    job_id INTEGER,
+    FOREIGN KEY (job_id) REFERENCES dim_jobs(id),
+    gender_id INTEGER,
+    FOREIGN KEY (gender_id) REFERENCES dim_genders(id),
+    isAdmin BOOLEAN
+);
 
+CREATE TABLE fact_users_interests (
+    id SERIAL PRIMARY KEY,
+    user_id INTEGER,
+    FOREIGN KEY (user_id) REFERENCES fact_users(id),
+    interest_id INTEGER,
+    FOREIGN KEY (interest_id) REFERENCES dim_interests(id)
+);
 
+CREATE TABLE fact_users_relactionship (
+    id SERIAL PRIMARY KEY,
+    user1_id INTEGER,
+    FOREIGN KEY (user1_id) REFERENCES fact_users(id),
+    user2_id INTEGER,
+    FOREIGN KEY (user2_id) REFERENCES fact_users(id),
+    created_date_id INTEGER,
+    FOREIGN KEY (created_date_id) REFERENCES dim_dates(id)
+);
 
-CREATE OR REPLACE FUNCTION insert_staging_browse_attractions()
-RETURNS TRIGGER
-LANGUAGE plpgsql
-AS $$
-    DECLARE
-        created_date_id INTEGER;
-    BEGIN
-        INSERT INTO dim_dates
-            (year, month, day)
-            VALUES (NEW.created_year, NEW.created_month, NEW.created_day)
-            ON CONFLICT(year, month, day)
-            DO NOTHING
-            RETURNING id
-            INTO created_date_id;
-        INSERT INTO fact_browse_attractions
-            (user_id, attraction_id, browse_count, created_date_id)
-            VALUES (NEW.user_id, NEW.attraction_id, NEW.browse_count, created_date_id);
-        RETURN NEW;
-    END
-$$;
+CREATE TABLE fact_browse_attractions (
+    id SERIAL PRIMARY KEY,
+    user_id INTEGER,
+    FOREIGN KEY (user_id) REFERENCES fact_users(id),
+    attraction_id INTEGER,
+    FOREIGN KEY (attraction_id) REFERENCES dim_attractions(id),
+    browse_count INTEGER,
+    created_date_id INTEGER,
+    FOREIGN KEY (created_date_id) REFERENCES dim_dates(id)
+);
 
-CREATE TRIGGER trigger_insert_staging_browse_attractions
-AFTER INSERT ON staging_browse_attractions
-FOR EACH ROW EXECUTE PROCEDURE insert_staging_browse_attractions();
+CREATE TABLE fact_posts (
+    id SERIAL PRIMARY KEY,
+    user_id INTEGER,
+    FOREIGN KEY (user_id) REFERENCES fact_users(id),
+    city_id INTEGER,
+    FOREIGN KEY (city_id) REFERENCES dim_cities(id),
+    attraction_id INTEGER,
+    FOREIGN KEY (attraction_id) REFERENCES dim_attractions(id),
+    created_date_id INTEGER,
+    FOREIGN KEY (created_date_id) REFERENCES dim_dates(id)
+);
 
+CREATE TABLE fact_browse_posts (
+    id SERIAL PRIMARY KEY,
+    user_id INTEGER,
+    FOREIGN KEY (user_id) REFERENCES fact_users(id),
+    browse_count INTEGER,
+    post_id INTEGER,
+    FOREIGN KEY (post_id) REFERENCES fact_posts(id)
+);
 
-
-
-CREATE OR REPLACE FUNCTION insert_staging_users_interests()
-RETURNS TRIGGER
-LANGUAGE plpgsql
-AS $$
-    BEGIN
-        INSERT INTO fact_users_interests
-            (user_id, interest_id)
-            VALUES (NEW.user_id, NEW.interest_id);
-        RETURN NEW;
-    END
-$$;
-
-CREATE TRIGGER trigger_insert_staging_users_interests
-AFTER INSERT ON staging_users_interests
-FOR EACH ROW EXECUTE PROCEDURE insert_staging_users_interests();
-
-
-
-
-CREATE OR REPLACE FUNCTION insert_db_posts()
-RETURNS TRIGGER
-LANGUAGE plpgsql
-AS $$
-    DECLARE
-        created_date_id INTEGER;
-    BEGIN
-        INSERT INTO dim_dates
-            (year, month, day)
-            VALUES (NEW.created_year, NEW.created_month, NEW.created_day)
-            ON CONFLICT(year, month, day)
-            DO NOTHING
-            RETURNING id
-            INTO created_date_id;
-        INSERT INTO fact_posts
-            (user_id, city_id, attraction_id, created_date_id)
-            VALUES (NEW.user_id, NEW.city_id, NEW.attraction_id, created_date_id);
-        RETURN NEstaging
-    END
-$$;
-
-CREATE TRIGGER trigger_insert_staging_posts
-AFTER INSERT ON staging_posts
-FOR EACH ROW EXECUTE PROCEDURE insert_staging_posts();
-
-
-
-
-CREATE OR REPLACE FUNCTION insert_staging_browse_posts()
-RETURNS TRIGGER
-LANGUAGE plpgsql
-AS $$
-    BEGIN
-        INSERT INTO fact_browse_posts
-            (user_id, post_id, browse_count)
-            VALUES (NEW.user_id, NEW.post_id, NEW.browse_count);
-        RETURN NEW;
-    END
-$$;
-
-CREATE TRIGGER trigger_insert_staging_browse_posts
-AFTER INSERT ON staging_browse_posts
-FOR EACH ROW EXECUTE PROCEDURE insert_staging_browse_posts();
-
-
-
-
-
-CREATE OR REPLACE FUNCTION insert_staging_posts_type()
-RETURNS TRIGGER
-LANGUAGE plpgsql
-AS $$
-    BEGIN
-        INSERT INTO fact_posts_type
-            (interest_id, post_id)
-            VALUES (NEW.interest_id, NEW.post_id);
-        RETURN NEW;
-    END
-$$;
-
-CREATE TRIGGER trigger_insert_staging_posts_type
-AFTER INSERT ON staging_posts_type
-FOR EACH ROW EXECUTE PROCEDURE insert_staging_posts_type();
+CREATE TABLE fact_posts_type (
+    id SERIAL PRIMARY KEY,
+    interest_id INTEGER,
+    FOREIGN KEY (interest_id) REFERENCES dim_interests(id),
+    post_id INTEGER,
+    FOREIGN KEY (post_id) REFERENCES fact_posts(id)
+);
