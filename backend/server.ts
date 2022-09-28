8da277183a7714568c1a7aa2274e9a2a6a8d20aa
Danny W <dannwtf@gmail.com>
2022-09-28 19:15:28 +0800

    update server cors

--- backend/server.ts
+++ backend/server.ts
@@ -100,7 +100,7 @@ const server = http.createServer(app);
 
 const io = new socketIO(server, {
     cors: {
-        origin: "https://localhost:3000",
+        origin: "*",
         methods: ["GET", "POST"],
     },
 });
