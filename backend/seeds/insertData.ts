0db27cc61230e8123bac941e67ceeaceca0bb509
爬山過海肥肥豬 <smartbean@KC-MacBook-Pro.local>
2023-01-19 15:09:48 -0500

    setup virtual environment + adjust dataToMong.pyo

--- backend/seeds/insertData.ts
+++ backend/seeds/insertData.ts
@@ -10,46 +10,46 @@ export async function seed(knex: Knex): Promise<void> {
 
     const countryId: Array<{ id: number }> = await knex("countries")
         .insert([
-            { name: "hiking", emergency_tel: "hiking.png" },
-            { name: "hiking", emergency_tel: "hiking.png" },
-            { name: "hiking", emergency_tel: "hiking.png" },
-            { name: "hiking", emergency_tel: "hiking.png" },
-            { name: "hiking", emergency_tel: "hiking.png" },
-            { name: "hiking", emergency_tel: "hiking.png" },
-            { name: "hiking", emergency_tel: "hiking.png" },
-            { name: "hiking", emergency_tel: "hiking.png" },
-            { name: "hiking", emergency_tel: "hiking.png" },
-            { name: "hiking", emergency_tel: "hiking.png" },
+            { name: "Australia", emergency_tel: "000" },
+            { name: "Canada", emergency_tel: "911" },
+            { name: "China", emergency_tel: "119" },
+            { name: "Denmark", emergency_tel: "112" },
+            { name: "Germany", emergency_tel: "112" },
+            { name: "Italy", emergency_tel: "112" },
+            { name: "Japan", emergency_tel: "110" },
+            { name: "Korea", emergency_tel: "hiking.png" },
+            { name: "UK", emergency_tel: "hiking.png" },
+            { name: "US", emergency_tel: "hiking.png" },
         ])
         .returning("name");
 
     const cityId: Array<{ id: number; name: string }> = await knex("cities")
         .insert([
-            { name: "hiking", description: "good" },
-            { name: "hiking", description: "good" },
-            { name: "hiking", description: "good" },
-            { name: "hiking", description: "good" },
-            { name: "hiking", description: "good" },
-            { name: "hiking", description: "good" },
-            { name: "hiking", description: "good" },
-            { name: "hiking", description: "good" },
-            { name: "hiking", description: "good" },
-            { name: "hiking", description: "good" },
+            { name: "hongkong", description: "good" },
+            { name: "hongkong", description: "good" },
+            { name: "hongkong", description: "good" },
+            { name: "hongkong", description: "good" },
+            { name: "hongkong", description: "good" },
+            { name: "hongkong", description: "good" },
+            { name: "hongkong", description: "good" },
+            { name: "hongkong", description: "good" },
+            { name: "hongkong", description: "good" },
+            { name: "hongkong", description: "good" },
         ])
         .returning("id");
 
     const attractionId: Array<{ id: number }> = await knex("attractions")
         .insert([
-            { name: "hiking" },
-            { name: "hiking" },
-            { name: "hiking" },
-            { name: "hiking" },
-            { name: "hiking" },
-            { name: "hiking" },
-            { name: "hiking" },
-            { name: "hiking" },
-            { name: "hiking" },
-            { name: "hiking" },
+            { name: "hiking", city_list: "hongkong" },
+            { name: "hiking", city_list: "hongkong" },
+            { name: "hiking", city_list: "hongkong" },
+            { name: "hiking", city_list: "hongkong" },
+            { name: "hiking", city_list: "hongkong" },
+            { name: "hiking", city_list: "hongkong" },
+            { name: "hiking", city_list: "hongkong" },
+            { name: "hiking", city_list: "hongkong" },
+            { name: "hiking", city_list: "hongkong" },
+            { name: "hiking", city_list: "hongkong" },
         ])
         .returning("id");
 
