b144a2633c6564932cc8b6b3f94bce66fd2b9b87
爬山過海肥肥豬 <smartbean@KC-MacBook-Pro.local>
2023-01-21 17:48:37 -0500

    App.tsx perf - added react.lazy

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,92 +1,120 @@
 import { Button } from '@chakra-ui/react'
-
 import { Link } from 'react-router-dom'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import React, { Suspense } from 'react'
 
-import Welcome from './components/welcome/Welcome'
-import Dashboard from './components/admin/contentDashboard/Dashboard'
-import ControlPanel from './components/admin/ControlPanel'
-import Landing from './components/landingPage/Landing'
-import SignUp from './components/auth/SignUp'
-import Forum from './components/forum/Forum'
-import Login from './components/auth/Login'
-import RequireAuth from './components/auth/RequireAuth'
-import InterestList from './components/matching/InterestList'
-
-import Friends from './components/friends/Friends'
-import Profile from './components/profile/Profile'
-import Home from './components/home/Home'
-import ManageUser from './components/admin/contentManageUser/ManageUser'
-import Matching from './components/matching/Matching'
-import MatchingSuccess from './components/matching/MatchingSuccess'
-import NewPost from './components/forum/NewPost'
-import MatchingIndex from './components/matching/MatchingIndex'
-import Skyscanner from './components/home/Skyscanner'
-import EditProfile from './components/profile/EditProfile'
-import Attraction from './components/home/Attraction'
-import ChatHome from './components/chat/ChatHome'
-import ChatRoom from './components/chat/ChatRoom'
-import Currency from './components/home/Currency'
-import SafetyContact from './components/safetyContact/SafetyContact'
-import EditInterest from './components/profile/EditInterest'
+//import component
+const Welcome = React.lazy(() => import('./components/welcome/Welcome'))
+const Dashboard = React.lazy(
+    () => import('./components/admin/contentDashboard/Dashboard')
+)
+const ControlPanel = React.lazy(() => import('./components/admin/ControlPanel'))
+const Landing = React.lazy(() => import('./components/landingPage/Landing'))
+const SignUp = React.lazy(() => import('./components/auth/SignUp'))
+const Forum = React.lazy(() => import('./components/forum/Forum'))
+const Login = React.lazy(() => import('./components/auth/Login'))
+const RequireAuth = React.lazy(() => import('./components/auth/RequireAuth'))
+const InterestList = React.lazy(
+    () => import('./components/matching/InterestList')
+)
+const Friends = React.lazy(() => import('./components/friends/Friends'))
+const Profile = React.lazy(() => import('./components/profile/Profile'))
+const Home = React.lazy(() => import('./components/home/Home'))
+const ManageUser = React.lazy(
+    () => import('./components/admin/contentManageUser/ManageUser')
+)
+const Matching = React.lazy(() => import('./components/matching/Matching'))
+const MatchingSuccess = React.lazy(
+    () => import('./components/matching/MatchingSuccess')
+)
+const NewPost = React.lazy(() => import('./components/forum/NewPost'))
+const MatchingIndex = React.lazy(
+    () => import('./components/matching/MatchingIndex')
+)
+const Skyscanner = React.lazy(() => import('./components/home/Skyscanner'))
+const EditProfile = React.lazy(() => import('./components/profile/EditProfile'))
+const Attraction = React.lazy(() => import('./components/chat/ChatHome'))
+const ChatHome = React.lazy(() => import('./components/home/Home'))
+const ChatRoom = React.lazy(() => import('./components/chat/ChatRoom'))
+const Currency = React.lazy(() => import('./components/home/Currency'))
+const SafetyContact = React.lazy(
+    () => import('./components/safetyContact/SafetyContact')
+)
+const EditInterest = React.lazy(
+    () => import('./components/profile/EditInterest')
+)
 
 function App() {
     return (
         <>
-            <Router>
-                <Routes>
-                    <Route path="/" element={<Landing />} />
-                    <Route path="welcome" element={<Welcome />} />
-                    <Route path="login" element={<Login />} />
-                    <Route path="signUp" element={<SignUp />} />
+            <React.Suspense fallback={<div>Loading...</div>}>
+                <Router>
+                    <Routes>
+                        <Route path="/" element={<Landing />} />
+                        <Route path="welcome" element={<Welcome />} />
+                        <Route path="login" element={<Login />} />
+                        <Route path="signUp" element={<SignUp />} />
 
-                    {/* Required Auth Route */}
-                    <Route path="/" element={<RequireAuth />}>
-                        <Route path="home" element={<Home />} />
-                        <Route path="attraction" element={<Attraction />} />
-                        <Route path="airline" element={<Skyscanner />} />
-                        <Route path="currency" element={<Currency />} />
-                        <Route path="contact" element={<SafetyContact />} />
-                        <Route path="forum" element={<Forum />} />
-                        <Route
-                            path="matchingIndex"
-                            element={<MatchingIndex />}
-                        />
-                        <Route path="interest" element={<InterestList />} />
-                        <Route path="matching" element={<Matching />} />
-                        <Route
-                            path="matchingSuccess"
-                            element={<MatchingSuccess />}
-                        />
-                        <Route path="newPost" element={<NewPost />} />
-                        <Route path="friends" element={<Friends />} />
-                        <Route path="profile" element={<Profile />} />
-                        <Route path="editProfile" element={<EditProfile />} />
-                        <Route path="editInterest" element={<EditInterest />} />
-                        <Route path="chat" element={<ChatHome />} />
-                        <Route path="chat/:room_id" element={<ChatRoom />} />
-                        <Route path="control" element={<ControlPanel />}>
-                            <Route index element={<Dashboard />} />
-                            <Route path="dashboard" element={<Dashboard />} />
-                            <Route path="user" element={<ManageUser />} />
-                            <Route path="forum" element={<ManageUser />} />
+                        {/* Required Auth Route */}
+                        <Route path="/" element={<RequireAuth />}>
+                            <Route path="home" element={<Home />} />
+                            <Route path="attraction" element={<Attraction />} />
+                            <Route path="airline" element={<Skyscanner />} />
+                            <Route path="currency" element={<Currency />} />
+                            <Route path="contact" element={<SafetyContact />} />
+                            <Route path="forum" element={<Forum />} />
+                            <Route
+                                path="matchingIndex"
+                                element={<MatchingIndex />}
+                            />
+                            <Route path="interest" element={<InterestList />} />
+                            <Route path="matching" element={<Matching />} />
+                            <Route
+                                path="matchingSuccess"
+                                element={<MatchingSuccess />}
+                            />
+                            <Route path="newPost" element={<NewPost />} />
+                            <Route path="friends" element={<Friends />} />
+                            <Route path="profile" element={<Profile />} />
+                            <Route
+                                path="editProfile"
+                                element={<EditProfile />}
+                            />
+                            <Route
+                                path="editInterest"
+                                element={<EditInterest />}
+                            />
+                            <Route path="chat" element={<ChatHome />} />
+                            <Route
+                                path="chat/:room_id"
+                                element={<ChatRoom />}
+                            />
+                            <Route path="control" element={<ControlPanel />}>
+                                <Route index element={<Dashboard />} />
+                                <Route
+                                    path="dashboard"
+                                    element={<Dashboard />}
+                                />
+                                <Route path="user" element={<ManageUser />} />
+                                <Route path="forum" element={<ManageUser />} />
+                            </Route>
                         </Route>
-                    </Route>
 
-                    <Route
-                        path="*"
-                        element={
-                            <>
-                                404 : Page Not Found
-                                <p></p>
-                                <Button>
-                                    <Link to="/">Back to Home</Link>
-                                </Button>
-                            </>
-                        }
-                    />
-                </Routes>
-            </Router>
+                        <Route
+                            path="*"
+                            element={
+                                <>
+                                    404 : Page Not Found
+                                    <p></p>
+                                    <Button>
+                                        <Link to="/">Back to Home</Link>
+                                    </Button>
+                                </>
+                            }
+                        />
+                    </Routes>
+                </Router>
+            </React.Suspense>
         </>
     )
 }
